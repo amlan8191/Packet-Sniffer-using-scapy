467e1aed36a0d3a429eb808d8bbc3efc5aff094f
amlan8191 <amlankalita05@gmail.com>
2025-06-07 11:32:05 +0530

    Sniffer program using scapy

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,20 +2,35 @@ from scapy.all import *
 
 try:
 
-	a=input("Enter the types of packets to be sniffed(tcp/udp/arp): ")
-	b=int(input("Enter the number of packets to be sniffed: "))
+	def send_packet(src_MAC,src_IP,dst_IP,iFace,payload):
+		pkt=Ether(src=src_MAC)/IP(src=src_IP,dst=dst_IP)/TCP()/Raw(load=payload)
+		ans,unans=srp(pkt,iface=iFace,timeout=3,verbose=0)
+		for sent,received in ans:
+			print("Response received from {received[IP].src}")
+			src_MAC=input("[+]Enter MAC address: ")
+			src_IP=input("[+]Enter source IP address: ")
+			dst_IP=input("[+]Enter destination IP address: ")
+			iFace=input("[+]Enter iface: ")
+			payload=input("[+]Enter payload: ")
 
-	Sniffer_packets=sniff(count=b,filter=a)
-	Sniffer_packets.show()
-	
-	c=input("Do you want to view the layers of a packet(y/n): ")
-	
+	send_packet(src_MAC,src_IP,dst_IP,iFace,payload)
+
 	while True:
-		
-		if c.lower()=='y':
-			d=int(input("Enter the index of the packet: "))
-			Sniffer_packets[d].show()
+		more=input("Do you want to send more packets?(y/n): ")
+		if more.lower()=="y":
+			req=input("Want to ammend changes in packets(y/n): ")
+			if req.lower()=="y":
+				src_MAC=input("[+]Enter MAC address: ")
+				src_IP=input("[+]Enter source IP address: ")
+				dst_IP=input("[+]Enter destination IP address: ")
+				iface=input("[+]Enter iface: ")
+				payload=input("[+]Enter payload: ")
+			else:
+				payload=input("[+]Enter payload: ")
+			send_packet(src_MAC,src_IP,dst_IP,iFace,payload)
 		else:
-			break
+			break 
 except KeyboardInterrupt:
-	print("\nSniffer program shutting down: ")
+	print("Sniffer program shutting down")
+	
+				
